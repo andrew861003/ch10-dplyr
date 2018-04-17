f693117f02db7707baa77630bb06ea82c91a85f1
Andrew <syw1003@uw.edu>
2018-04-16 17:00:24 -0700

    completed

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -2,43 +2,57 @@
 
 # Install the `nycflights13` package. Load (`library()`) the package.
 # You'll also need to load `dplyr`
-
-
+install.packages("nycflights13")
+library("nycflights13")
+library(dplyr)
+View(flights)
 # The data frame `flights` should now be accessible to you.
 # Use functions to inspect it: how many rows and columns does it have?
 # What are the names of the columns?
 # Use `??flights` to search for documentation on the data set (for what the 
 # columns represent)
-
-
+nrow(flights)
+ncol(flights)
+colnames(flights)
 # Use `dplyr` to give the data frame a new column that is the amount of time
 # gained or lost while flying (that is: how much of the delay arriving occured
 # during flight, as opposed to before departing).
-
+flights_data <- mutate(flights, time_gain_or_lost = arr_delay - dep_delay)
 
 # Use `dplyr` to sort your data frame in descending order by the column you just
 # created. Remember to save this as a variable (or in the same one!)
-
+sorted_flights_data <- arrange(flights_data, time_gain_or_lost)
 
 # For practice, repeat the last 2 steps in a single statement using the pipe
 # operator. You can clear your environmental variables to "reset" the data frame
-
+flights_data <- flights %>%
+  mutate(time_gain_or_lost = arr_delay - dep_delay) %>%
+  arrange(time_gain_or_lost)
 
 # Make a histogram of the amount of time gained using the `hist()` function
-
+hist(flights_data$time_gain_or_lost)
 
 # On average, did flights gain or lose time?
 # Note: use the `na.rm = TRUE` argument to remove NA values from your aggregation
-
+summarize(flights_data, avg = mean(time_gain_or_lost, na.rm = TRUE)) > 0
 
 # Create a data.frame of flights headed to SeaTac ('SEA'), only including the
 # origin, destination, and the "gain_in_air" column you just created
-
+sea <- flights_data %>%
+  filter(dest == "SEA") %>%
+  select(origin, dest, time_gain_or_lost)
 
 # On average, did flights to SeaTac gain or loose time?
-
+summarize(sea, avg = mean(time_gain_or_lost, na.rm = TRUE)) > 0
 
 # Consider flights from JFK to SEA. What was the average, min, and max air time
 # of those flights? Bonus: use pipes to answer this question in one statement
 # (without showing any other data)!
-
+jfk_sea <- flights %>%
+  filter(origin == "JFK",
+         dest == "SEA") %>%
+  select(air_time) %>%
+  summarize( avg = mean(air_time, na.rm = TRUE),
+             min = min(air_time, na.rm = TRUE),
+             max = max(air_time, na.rm = TRUE)
+             )
